6c308d9acdb4967b056646e9d1d4364de33ba86d
Aleksey Izmailov <izmailoff@users.noreply.github.com>
2015-11-17 18:36:57 +0800

    Update P09.scala

--- src/main/scala/s99/p09/P09.scala
+++ src/main/scala/s99/p09/P09.scala
@@ -2,7 +2,15 @@ package s99.p09
 
 object P09 {
 
-  def pack[T](input: List[T]): List[List[T]] = {
+  def pack[T](input: List[T]): List[List[T]] =
+    input.foldRight(Nil : List[List[T]]) ((e, accu) => accu match {
+      case Nil => List(List(e))
+      case curList@(h :: t) if e == h => List(e) :: curList
+      case curList@(h :: t) => List(List(e)) ::: curList
+  })
+
+  // no helper functions used like fold:
+  def packOld[T](input: List[T]): List[List[T]] = {
     def packWithPrevious(remaining: List[T])(previous: List[T]): List[List[T]] =
       remaining match {
         case List() => List(previous)
